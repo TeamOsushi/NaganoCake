4c2633fc42d2537eee3193608f5fedbd74ef0fd4
EC2 Default User <ec2-user@ip-172-31-4-180.ec2.internal>
2021-12-15 02:26:16 +0000

    カート機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cart_items,only: [:index,:update,:create,:destroy] do
+        collection do
+          delete '/' => 'cart_items#all_destroy'
+        end
+      end
 end
