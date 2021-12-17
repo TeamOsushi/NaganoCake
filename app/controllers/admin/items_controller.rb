adec9bcbc0f632e590dd38ae881d5ffdf1965975
jin <jin.10319922@gmail.com>
2021-12-17 04:04:03 +0000

    [管理者側のview]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ItemsController < ApplicationController
+    def top
+        
+    end
+    
+    def index
+     @items = Item.all
+    end
+    
+    def show
+     @item = Item.find(params[:id])
+    end
+    
+    def new
+     @item = Item.new
+    end
+    
+    def create
+      cart = Cart_item.new(params[:id])
+      cart.save
+      #ここに商品詳細に飛ぶリンク
+    end
+    
+    
+    def item_params
+     params.require(:item).permit(:item_name, :image, :genre, :introduction, :is_sales_status, :price_without_tax)
+    end 
+
+
 end
