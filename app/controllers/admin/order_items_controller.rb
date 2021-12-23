3838fe34d310015d6c51af48d11fb0330f1ccda0
jin <jin.10319922@gmail.com>
2021-12-23 09:03:14 +0000

    [商品ステータス変更]

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrderItemsController < ApplicationController
     
 def update
-	@order_item = OrderItem.find(params[:id])
+	@order_item = OrderItem.find(params[:order_id])
 	@order = @order_item.order
     @create_status = params[:order_item][:create_status].to_i
     if @order_item.update(order_item_params)
@@ -13,7 +13,7 @@ end
 
 	private
 	def order_item_params
-	    params.require(:order_item).permit(:create_status, :id)
+	    params.require(:order_item).permit(:create_status, :id, :order_id)
 	end
 	
 end
